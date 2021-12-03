88cc400e4e65da1f114317586da71b201000b348
bassidus <basse1978@gmail.com>
2021-12-03 22:45:31 +0100

    day3

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@
             var sw = new Stopwatch();
             var day1 = new Day1();
             var day2 = new Day2();
+            var day3 = new Day3();
 
             sw.Start();
 
@@ -15,9 +16,11 @@
             Console.WriteLine($"Day 1 - Part 2: {day1.Part2()}");
             Console.WriteLine($"Day 2 - Part 1: {day2.Part1()}");
             Console.WriteLine($"Day 2 - Part 2: {day2.Part2()}");
-            
+            Console.WriteLine($"Day 3 - Part 1: {day3.Part1()}");
+            Console.WriteLine($"Day 3 - Part 2: {day3.Part2()}");
+
             sw.Stop();
-            
+
             Console.WriteLine($"Total: {sw.Elapsed}");
         }
     }
