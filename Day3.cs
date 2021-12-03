d807e1fd35297031bcced7ef40f0bab337a40283
bassidus <basse1978@gmail.com>
2021-12-03 22:45:07 +0100

    day3

---
+++ Day3.cs
@@ -0,0 +1,50 @@
+namespace AdventOfCode
+{
+    public class Day3
+    {
+        private string[] logbook = File.ReadAllLines("./input/day3.txt");
+        private int BinaryToInt(string b) => Convert.ToInt32(b, 2);
+        private enum Rating { OxygenRating, Co2Rating }
+        public int Part1()
+        {
+            var gamma = string.Empty;
+            var epsilon = string.Empty;
+
+            for (var i = 0; i < 12; i++)
+            {
+                var ones = 0;
+                var zeroes = 0;
+
+                foreach (var log in logbook)
+                {
+                    ones += log[i] == '1' ? 1 : 0;
+                    zeroes += log[i] == '0' ? 1 : 0;
+                }
+
+                gamma += ones > zeroes ? "1" : "0";
+                epsilon += zeroes > ones ? "1" : "0";
+            }
+
+            return BinaryToInt(gamma) * BinaryToInt(epsilon);
+        }
+        public int Part2() => Search(Rating.OxygenRating) * Search(Rating.Co2Rating);
+        private int Search(Rating rating)
+        {
+            var oxygenOrCO2 = rating == Rating.OxygenRating ? true : false;
+            var logs = logbook.ToList();
+            foreach (var i in Enumerable.Range(0, logbook[0].Length))
+            {
+                var bits = logs.Count(log => log[i] == '1');
+                var commonBit = (2 * bits >= logs.Count) == oxygenOrCO2 ? '1' : '0';
+
+                logs = logs.Where(log => log[i] == commonBit).ToList();
+
+                if (logs.Count == 1)
+                {
+                    return BinaryToInt(logs[0]);
+                }
+            }
+            return 0;
+        }
+    }
+}
